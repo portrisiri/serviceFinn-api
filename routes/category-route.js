ad7b4b05553ce8d95d64d5e02cb87d5be3947804
portrisiri <ranawat@Pors-Air.home>
2025-03-12 17:40:35 +0700

    Create schema for category controller, and add mock data for database seeding

--- routes/category-route.js
+++ routes/category-route.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const categoryController = require('../controllers/category-controller');
+const { bodyValidator } = require('../validators/body-validator');
+const { createSubCategorySchema } = require('../validators/category-schema');
 
 const categoryRoute = express.Router();
 
-categoryRoute.post('/', categoryController.getCategories);
+// http://localhost:4289/category/
+categoryRoute.get('/', categoryController.getCategories);
+
+// http://localhost:4289/category/subcategory
+categoryRoute.get('/subcategory', categoryController.getSubCategories);
+
+// http://localhost:4289/category/:categoryId
+categoryRoute.get('/:categoryId', categoryController.getSubCatByCat);
+
+// http://localhost:4289/category/
+categoryRoute.post('/', bodyValidator(createSubCategorySchema), categoryController.createSubCategory);
+
+// http://localhost:4289/category/:subCatId
+categoryRoute.delete('/:subCatId', categoryController.deleteSubCat);
 
 module.exports = categoryRoute;
--- validators/category-schema.js
+++ validators/category-schema.js
@@ -1,1 +1,8 @@
-console.log('Sample Schema');
+const { z } = require('zod');
+
+exports.createSubCategorySchema = z.object({
+  categoryId: z.number(),
+  categoryName: z.string(),
+  subCatId: z.number(),
+  subCatName: z.string(),
+});
